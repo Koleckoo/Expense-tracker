34e824066b0d492bbf512cd1f4ae7ca681e1f081
jan.illetsko@greyson.eu <jan.illetsko@greyson.eu>
2023-10-06 10:33:30 +0200

    validation and submission added

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -10,6 +10,10 @@ function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
+  const selectedExpense = expensesCtx.expenses.find(
+    (expense) => expense.id === editedExpenseId
+  );
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: isEditing ? "Edit Expense" : "Add Expense",
@@ -23,19 +27,11 @@ function ManageExpense({ route, navigation }) {
   function cancelHandler() {
     navigation.goBack();
   }
-  function confirmHandler() {
+  function confirmHandler(expenseData) {
     if (isEditing) {
-      expensesCtx.updateExpense(editedExpenseId, {
-        description: "Testupdate",
-        amount: 32.78,
-        date: new Date("2023-10-06"),
-      });
+      expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
-      expensesCtx.addExpense({
-        description: "Test",
-        amount: 34.78,
-        date: new Date("2023-10-05"),
-      });
+      expensesCtx.addExpense(expenseData);
     }
     navigation.goBack();
   }
@@ -45,7 +41,9 @@ function ManageExpense({ route, navigation }) {
       <ExpenseForm
         onCancel={cancelHandler}
         isEditing={isEditing}
+        onSubmit={confirmHandler}
         submitButtonLabel={isEditing ? "Update" : "Add"}
+        defaultValues={selectedExpense}
       />
 
       {isEditing && (
